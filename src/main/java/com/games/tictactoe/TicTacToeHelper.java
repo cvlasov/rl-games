bdb3f86686692d002555f514e8904ad910ef1cc0
cvlasov <catherinev.canada@gmail.com>
2018-02-07 23:47:47 +0000

    Adding methods to TicTacToeHelper

--- src/main/java/com/games/tictactoe/TicTacToeHelper.java
+++ src/main/java/com/games/tictactoe/TicTacToeHelper.java
@@ -57,6 +57,34 @@ public final class TicTacToeHelper {
   /** Return for an action that does not cause the game to end. */
   public static final int GAME_IN_PROGRESS_RETURN = 0;
 
+  public static void print(List<TokenType> grid) {
+    System.out.println(" " + grid.get(0) + " | " + grid.get(1) + " | " + grid.get(2));
+    System.out.println("-----------");
+    System.out.println(" " + grid.get(3) + " | " + grid.get(4) + " | " + grid.get(5));
+    System.out.println("-----------");
+    System.out.println(" " + grid.get(6) + " | " + grid.get(7) + " | " + grid.get(8));
+    System.out.println();
+  }
+
+  /**
+   * Returns a hash code of a Tic-Tac-Toe grid in such a way that two states
+   * have the same hash code if and only if they have the same tokens, or lack
+   * thereof, at the exact same indices (i.e. no symmetry is taken into
+   * account).
+   */
+  public static int standardHashCode(List<TokenType> grid) {
+    int hash = 3 * grid.get(0).hashCode();
+    hash +=    5 * grid.get(1).hashCode();
+    hash +=    7 * grid.get(2).hashCode();
+    hash +=   11 * grid.get(3).hashCode();
+    hash +=   13 * grid.get(4).hashCode();
+    hash +=   17 * grid.get(5).hashCode();
+    hash +=   19 * grid.get(6).hashCode();
+    hash +=   23 * grid.get(7).hashCode();
+    hash +=   29 * grid.get(8).hashCode();
+    return hash;
+  }
+
   public static List<TokenType> flipGridVertically(List<TokenType> g) {
     List<TokenType> verticalFlipGrid = new ArrayList<>();
     verticalFlipGrid.add(g.get(6));
