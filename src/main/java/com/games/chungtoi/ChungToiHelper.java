1fd99a1f0934aecc98ec24e80002b9189976b193
cvlasov <catherinev.canada@gmail.com>
2018-02-22 02:27:46 +0000

    Making Chung Toi grid printing nicer

--- src/main/java/com/games/chungtoi/ChungToiHelper.java
+++ src/main/java/com/games/chungtoi/ChungToiHelper.java
@@ -75,18 +75,18 @@ public class ChungToiHelper {
 
   public static void print(List<TokenType> grid) {
     StringBuilder builder = new StringBuilder();
-    builder.append(grid.get(0).toString() + "*");
-    builder.append(grid.get(1).toString() + "*");
-    builder.append(grid.get(2).toString() + "\n");
-    builder.append("***********\n");
-    builder.append(grid.get(3).toString() + "*");
-    builder.append(grid.get(4).toString() + "*");
-    builder.append(grid.get(5).toString() + "\n");
-    builder.append("***********\n");
-    builder.append(grid.get(6).toString() + "*");
-    builder.append(grid.get(7).toString() + "*");
-    builder.append(grid.get(8).toString() + "\n");
-    System.out.print(builder.toString());
+    builder.append(" " + grid.get(0).toString() + " *");
+    builder.append(" " + grid.get(1).toString() + " *");
+    builder.append(" " + grid.get(2).toString() + "\n");
+    builder.append("*****************\n");
+    builder.append(" " + grid.get(3).toString() + " *");
+    builder.append(" " + grid.get(4).toString() + " *");
+    builder.append(" " + grid.get(5).toString() + "\n");
+    builder.append("*****************\n");
+    builder.append(" " + grid.get(6).toString() + " *");
+    builder.append(" " + grid.get(7).toString() + " *");
+    builder.append(" " + grid.get(8).toString() + "\n");
+    System.out.println(builder.toString());
   }
 
   public static boolean threeInARow(
