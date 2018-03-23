9ee8f4b5e51a1ed9ab52ee8abc01d084fe6e25b1
cvlasov <catherinev.canada@gmail.com>
2018-03-23 10:05:56 -0400

    Ordering State interface methods alphabetically

--- src/main/java/com/games/general/State.java
+++ src/main/java/com/games/general/State.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 /** Interface for the state of a game. */
 public interface State {
-  List<Action> getActions();
   State applyAction(Action a);
+  List<Action> getActions();
   boolean isTerminalState();
   void print();
 }
