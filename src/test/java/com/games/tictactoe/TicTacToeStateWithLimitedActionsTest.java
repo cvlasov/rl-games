9c2f86b850fca3bf6dcc77e0661544e57499fda9
cvlasov <catherinev.canada@gmail.com>
2018-04-09 12:50:18 +0100

    Adding new test to check that specific states are not reched

--- src/test/java/com/games/tictactoe/TicTacToeStateWithLimitedActionsTest.java
+++ src/test/java/com/games/tictactoe/TicTacToeStateWithLimitedActionsTest.java
@@ -1,16 +1,23 @@
 package com.games.tictactoe;
 
 import static com.games.tictactoe.TicTacToeHelper.GRID_SIZE;
+import static org.hamcrest.collection.IsMapContaining.hasKey;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
+import com.games.agents.MonteCarloAgent;
+import com.games.agents.RandomAgent;
 import com.games.general.Action;
+import com.games.general.State;
 import com.games.tictactoe.TicTacToeAction;
 import com.games.tictactoe.TicTacToeHelper.Player;
 import com.games.tictactoe.TicTacToeHelper.TokenType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -92,5 +99,80 @@ public class TicTacToeStateWithLimitedActionsTest {
      Set<Action> expectedActions = new HashSet<>();
 
      assertThat(new HashSet<>(state.getActions()), equalTo(expectedActions));
+  }
+
+  @Test
+  public void testStatesNotInMonteCarloPolicy() {
+    MonteCarloAgent mcAgent = new MonteCarloAgent(0.7);
+    RandomAgent randAgent = new RandomAgent();
+    TicTacToeGameWithLimitedActions game =
+        new TicTacToeGameWithLimitedActions(mcAgent, randAgent);
+
+    for (int i = 0 ; i < 10*1000 ; i++) {
+      game.play();
+    }
+
+    TicTacToeNormalState state1 = new TicTacToeNormalState(
+        Arrays.asList(new TokenType[] {
+          TokenType.NONE, TokenType.X,    TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE
+        }),
+        Player.O);
+
+    TicTacToeNormalState state2 = new TicTacToeNormalState(
+        Arrays.asList(new TokenType[] {
+          TokenType.NONE, TokenType.NONE, TokenType.X,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE
+        }),
+        Player.O);
+
+    TicTacToeNormalState state3 = new TicTacToeNormalState(
+        Arrays.asList(new TokenType[] {
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.X,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE
+        }),
+        Player.O);
+
+    TicTacToeNormalState state4 = new TicTacToeNormalState(
+        Arrays.asList(new TokenType[] {
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.X,    TokenType.NONE, TokenType.NONE
+        }),
+        Player.O);
+
+    TicTacToeNormalState state5 = new TicTacToeNormalState(
+        Arrays.asList(new TokenType[] {
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.X,    TokenType.NONE
+        }),
+        Player.O);
+
+    TicTacToeNormalState state6 = new TicTacToeNormalState(
+        Arrays.asList(new TokenType[] {
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.NONE,
+          TokenType.NONE, TokenType.NONE, TokenType.X
+        }),
+        Player.O);
+
+    List<State> states = new ArrayList<>();
+    states.add(state1);
+    states.add(state2);
+    states.add(state3);
+    states.add(state4);
+    states.add(state5);
+    states.add(state6);
+
+    System.out.println(
+        String.format("Policy size: %d", mcAgent.getPolicy().size()));
+
+    for (State s : states) {
+      assertThat(mcAgent.getPolicy(), not(hasKey(s)));
+    }
   }
 }
